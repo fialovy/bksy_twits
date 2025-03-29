c298e5949bbc0edb62d253f1823d79833492145a
Norm Julian <normvjulian@gmail.com>
2025-03-28 21:55:57 -0600

    add villain quotes

---
+++ villain_quotes.py
@@ -0,0 +1,82 @@
+# I don't need the keys, but I wanna be transparent :)
+VILLAIN_QUOTES = {
+    "evil_queen": [
+        "Mirror, mirror on the wall, who is the fairest of them all?",
+    ],
+    "queen_of_hearts": [
+        "Off wit their heads!",
+        "I warn you, child. If I lose my temper, you lose your head! Understand?!",
+        "Off with their heads!",
+        "Sentence first — verdict afterwards.",
+        "Who's been painting my roses red?!",
+    ],
+    "hans": [
+        "If only there was someone out there who loved you.",
+    ],
+    "gothel": [
+        "Oh, great. Now I'm the bad guy.",
+        "Mother knows best.",
+    ],
+    "ernesto": [
+        "You have to do whatever it takes to seize your moment.",
+    ],
+    "facilier": [
+        "C’mon, boys, won’t you shake a poor sinner's hand?",
+        "Besides, you and I both know the real power in this world ain't magic. It's money! Buckets of it.",
+        "Aren’t you tired of living on the margins while all the fat cats in the world enjoy the good life?",
+        "I got friends on the other side.",
+    ],
+    "ursula": [
+        "I’ll admit that in the past I've been a nasty.",
+        "I’ll see him wriggle like a worm on a hook!",
+        "Poor unfortunate souls, in pain, in need!",
+    ],
+    "jafar": [
+        "You've heard of the golden rule, haven't you? Whoever has the gold makes the rules!",
+        "The power! The absolute power!",
+    ],
+    "maleficent": [
+        "You poor, simple fools. Thinking you could defeat me. Me! The mistress of all evil!",
+    ],
+    "oogie_boogie": [
+        "I am the shadow on the moon at night, filling your dreams to the brim with fright",
+        "I'm the Oogie Boogie Man!",
+    ],
+    "gaston": [
+        "I use antlers in all of my decorating!",
+        "And don’t I deserve the best?",
+        "I'm especially good at expectorating!",
+    ],
+    "kaa": [
+        "Trust in me.",
+    ],
+    "darla": [
+        "I'm a piranha! They live in the Amazon!",
+    ],
+    "bellwether": [
+        "Fear always works!",
+        "You think I'm going to let you make me the villain?",
+    ],
+    "stromboli": [
+        "You will make lots of money… For me!",
+    ],
+    "cruella_de_vil": [
+        "I live for furs. I worship furs!",
+        "I don't care how you kill the little beasts, just do it!",
+    ],
+    "yzma": ["Wrong lever!", "Why do we even have that lever?!"],
+    "lotso": ["No one cares about toys like us anymore."],
+    "syndrome": [
+        "You sly dog! You got me monologuing!",
+        "I invented weapons. Now I have a whole line of them!",
+    ],
+    "shego": [
+        "Ugh, do you know how hard it is to keep your hair decent when you're surrounded by buffoons?",
+        "Please. I break things, not fix them.",
+    ],
+    "rattigan": ["Oh, I love it when I'm nasty!", "I am not a rat!"],
+    "plankton": [
+        "I went to college!",
+        "Goodbye, everyone! I’ll remember you all in therapy!",
+    ],
+}
